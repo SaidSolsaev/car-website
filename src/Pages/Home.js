6bce8c96efdbd5ef8c11646f3c5f87b88027607e
Said <said095@hotmail.com>
2023-06-18 17:07:52 +0200

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,59 @@
-import React from 'react'
-import Input from '../Components/Input'
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components'
+import { Form, Button} from 'react-bootstrap'
+import XMLParser from "react-xml-parser";
 
 export default function Home() {
+    const [reg, setReg] = useState("")
+    const [car, setCar] = useState([])
+
+    // const fetch = require('node-fetch');
+
+    const url = `https://www.regcheck.org.uk/api/reg.asmx/CheckNorway?RegistrationNumber=${reg}&username=saidsolsaev`;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            
+        }
+    };
+
+    const getCar = () =>{
+    
+        fetch(url, options)
+            .then(res => res.text())
+            .then(data => {
+                var xml = new XMLParser().parseFromString(data);
+                console.log(xml)
+            })
+            .catch(err => console.error('error:' + err));
+            console.log(car)
+    }
+
+
+    // useEffect(() => {
+    //     getCar()
+    // }, [])
+
     return (
-        <div>
-            <Input placeholder="Registreings Nr..."/>
-        </div>
+        <Container>
+            <Form>
+                <Form.Group>
+                    <Form.Control type="text" placeholder="Reg. nr" value={reg} onChange={(e) => setReg(e.target.value)}/>
+                </Form.Group>
+                <Button onClick={getCar}>Søk</Button>
+            </Form>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 25%;
+    padding: 20px;
+    form{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+    }
+`;
